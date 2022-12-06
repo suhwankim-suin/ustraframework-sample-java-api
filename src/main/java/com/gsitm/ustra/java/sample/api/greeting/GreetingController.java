2c3a5d0ac7d84800e608e3f6b4be8656fed69a0f
KIMSUHWAN <KIMSUHWAN@DESKTOP-S5JQFFM>
2022-12-06 20:10:14 +0900

    개발중 develop branch 수정됨

--- src/main/java/com/gsitm/ustra/java/sample/api/greeting/GreetingController.java
+++ src/main/java/com/gsitm/ustra/java/sample/api/greeting/GreetingController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/greeting")
 public class GreetingController {
 	/*
-	 * merge commit
+	 *  개발중 develop branch 수정됨
 	 *
 	 */
     @GetMapping
